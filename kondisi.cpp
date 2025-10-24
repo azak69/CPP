f6ea3c9c82ce11e274bc3bab76be81b6caf56544
Azaki <zakidawud69@gmail.com>
2025-10-24 11:55:16 +0700

    Dasar

--- kondisi.cpp
+++ kondisi.cpp
@@ -9,7 +9,9 @@ int main(){
 	hasil = a % 2;
 	
 	string kondisi = hasil == 1 ? "Ganjil" : "Genap";
-	cout << "Angka " << a << " adalah bilangan " << kondisi << endl;
+	cout << "Angka " << a << " adalah bilangan " << kondisi << "." << endl;
 	
 	return 0;
 }
+
+// Program ini menentukan apakah sebuah angka yang dimasukkan oleh pengguna adalah bilangan ganjil atau genap.
