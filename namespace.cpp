6c21be57e2e0eaf6b2e2b7d9af5c3e5c2c97ad41
Azaki <zakidawud69@gmail.com>
2025-10-31 17:26:49 +0700

    Dasar

---
+++ namespace.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+namespace bilangan{
+    void cetak(int a){
+    std::cout << a << std::endl;
+    }
+}
+
+namespace teks{
+    void cetak(std::string a){
+    std::cout << a << std::endl;
+    }
+}
+
+// int main(){
+//     bilangan::cetak(69);
+//     teks::cetak("Kedamaian Abadi");
+//     return 0;
+// }
+
+using namespace bilangan;
+using namespace teks;
+
+int main(){
+    cetak(69);
+    cetak("Kedamaian Abadi");
+    return 0;
+}
