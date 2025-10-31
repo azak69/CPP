136be30fec203bd8b12132596c9025bdc6a63a59
Azaki <zakidawud69@gmail.com>
2025-10-31 19:57:03 +0700

    Dasar

---
+++ for_loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int awal, akhir;
+    cout << "Masukkan angka awal: ";
+    cin >> awal;
+    cout << "Masukkan angka akhir: ";
+    cin >> akhir;
+    for(int i = awal; i <= akhir; i++){
+        cout << "Perulangan ke-" << i << endl;
+    }
+    return 0;
+}
+
+// Program ini mendemonstrasikan penggunaan perulangan for di C++.
