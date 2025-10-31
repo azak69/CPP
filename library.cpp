b272374def71e05273addbd0a749421368233018
Azaki <zakidawud69@gmail.com>
2025-10-31 17:37:58 +0700

    Dasar

---
+++ library.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    double a = pow(2.0, 3.0);
+    cout << "2 pangkat 3 adalah: " << a << endl;
+    double b = sqrt(16.0);
+    cout << "Akar kuadrat dari 16 adalah: " << b << endl;
+    cout << "Angka random : " << rand() % 100 + 1 << endl;
+    return 0;
+}
+ // Program ini mendemonstrasikan penggunaan beberapa fungsi dari library standar C++.
