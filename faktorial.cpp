e8a91c4738dcf6b8a330e6d1ec12806fc978575b
Azaki <zakidawud69@gmail.com>
2025-11-03 20:15:18 +0700

    Dasar

---
+++ faktorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int faktorial(int n){
+    if (n <= 1){
+        return n;
+    }else{
+        return n * faktorial(n - 1);
+    }
+}
+
+int main(){
+    int a;
+    cout << "Masukkan angka yang mau difaktorial : "; cin >> a;
+    cout << "=======================================" << endl;
+    cout << "Faktorial dari " << a << " adalah " << faktorial(a) << endl;
+}
+
+// Program ini menghitung faktorial dari suatu angka dengan menggunakan rekursif.
