916df4668e6931499eb7def1f4a1e85d8eda8217
Azaki <zakidawud69@gmail.com>
2025-11-06 07:44:01 +0700

    Dasar

--- pointer.cpp
+++ pointer.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void angka(int *y){
+    cout << "Alamat parameter : " << y << endl;
+    cout << "Nilai parameter : " << *y << endl;
+}
+
+void kuadrat(int *z){
+    *z = *z * *z;
+}
+
 int main(){
     int a = 9;
     int b;
@@ -11,6 +20,11 @@ int main(){
     cout << "Alamat b : " << &b << endl;
     cout << "c menunjukkan alamat a : " << c << endl;
     cout << "d menunjukkan nilai dari variabel yang ditunjuk c, yaitu a : " << d << endl;
+    
+    int x = 5;
+    angka(&x);
+    kuadrat(&x);
+    cout << "Hasil kuadrat : " << x << endl;
     return 0;
 }
 
