5b272c0153aac9943e6092da03141eae5801fe94
Azaki <zakidawud69@gmail.com>
2025-11-13 08:21:00 +0700

    Dasar

---
+++ realloc.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main (){
+    int *a = (int*) malloc(5 *sizeof(int));
+
+     if (a == NULL){
+        cout << "Alokasi memori gagal!" << endl;
+        return 1;
+    }
+
+    for (int i = 0; i < 5; i++){
+        a[i] = i + 1;
+    }
+
+    cout << "Sebelumnya : ";
+    for (int j = 0; j < 5; j++){
+        cout << a[j] << " ";
+    }
+
+    a = (int*) realloc(a, 10 * sizeof(int));
+
+    for (int i = 0; i < 10; i++){
+        a[i] = i + 1;
+    }
+
+    cout << "\nSetelah realloc : ";
+    for (int j = 0; j < 10; j++){
+        cout << a[j] << " ";
+    }
+}
+
+// Program ini mendemonstrasikan penggunaan realloc dalam library cstdlib untuk mengubah jumlah ruang memori yang sudah dialokasikan dengan malloc.
